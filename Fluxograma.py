9dc15d7d6bae2d2dacc3e8ca6bbdb8a55dc5cca0
Shaco23 <af553345@gmail.com>
2024-08-22 22:01:31 -0300

    atualização do Arquivo Readme

---
+++ Fluxograma.py
@@ -0,0 +1,10 @@
+numero = input("Digite um numero para validação de positvo/negativo")
+
+numero = int(numero)
+
+if numero > 0:
+    print("Numero é positivo")
+elif numero < 0:
+    print("Numero é negativo")
+else:
+    print("Numero é zero")
